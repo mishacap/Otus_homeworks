7924a0db27b0b13a70fd922d93da9eaee0a9013e
mishacap <trustredrum@gmail.com>
2024-08-10 10:50:21 +0300

    fix cart top test

--- tests/test_opencart.py
+++ tests/test_opencart.py
@@ -20,123 +20,139 @@ from page_objects.alert_element import AlertSuccessElement
 
 
 
-# def test_top_elements(browser, base_url):
-#     browser.get(f"{base_url}:8081")
-#     WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#top > div > div.nav.float-start")))
-#     WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#search > button")))
-#     WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#search > input")))
-#     WebDriverWait(browser, 3).until(EC.presence_of_element_located(
-#         (By.CSS_SELECTOR, "#category")))
-#     WebDriverWait(browser, 2 ).until(
-#         method=EC.title_is("Your Store"),
-#         message="Не дождался корректного тайтла страницы"
-#     )
-#
-#
-# def test_administration_elements(browser, base_url):
-#     browser.get(f"{base_url}:8081/administration/")
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#input-username")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#input-password")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#form-login > div.text-end > button")))
-#
-#
-# def test_registration_elements(browser, base_url):
-#     browser.get(f"{base_url}:8081/index.php?route=account/register")
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#input-firstname")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#input-email")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#input-password")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#input-newsletter")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#form-register > div > button")))
-#
-#
-# def test_product_elements(browser, base_url):
-#     browser.get(f"{base_url}:8081/en-gb/product/apple-cinema")
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(1) > div > a > img")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(1) > div")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(2) > form > div > button:nth-child(1)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(2) > form > div > button:nth-child(2)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#button-cart")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR,
-#          "#content > div.row.mb-3 > div:nth-child(2) > ul:nth-child(3) > li:nth-child(2) > h2 > span")))
-#     WebDriverWait(browser, 2 ).until(
-#         method=EC.title_is("Apple Cinema 30"),
-#         message="Не дождался корректного тайтла страницы"
-#     )
-#
-#
-# def test_catalog_elements(browser, base_url):
-#     browser.get(f"{base_url}:8081/en-gb/catalog/")
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(1)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(2)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(3)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(4)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(5)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(6)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(7)")))
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(8)")))
-#
-#
-# def test_login_logout_administration(browser, base_url):
-#     browser.get(f"{base_url}:8081/administration/")
-#     input_username = browser.find_element(By.ID, "input-username")
-#     input_username.send_keys("user")
-#     input_password = browser.find_element(By.ID, "input-password")
-#     input_password.send_keys("bitnami")
-#     browser.find_element(By.CSS_SELECTOR, "#form-login > div.text-end > button").click()
-#     WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
-#         (By.CSS_SELECTOR, "#nav-profile > a > span")))
-#     browser.find_element(By.CSS_SELECTOR, "#nav-logout > a > span").click()
-
-
-def test_add_to_cart(browser, base_url, db_connection):
+def test_top_elements(browser, base_url):
     browser.get(f"{base_url}:8081")
-    product_name = MainPage(browser).get_featured_product_name(1)
-    MainPage(browser).click_featured_product(1)
-    ProductPage(browser).add_to_cart()
-    AlertSuccessElement(browser).shopping_cart.click()
-    CartPage(browser) \
-        .wait_for_product_in_cart(product_name) \
-        .click_checkout()
-    CheckoutPage(browser).click_login_page_link()
-    UserPage(browser).login(*create_random_user(db_connection))
-    CheckoutPage(browser).wait_page_load()
+    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#top > div > div.nav.float-start")))
+    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#search > button")))
+    WebDriverWait(browser, 3).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#search > input")))
+    WebDriverWait(browser, 3).until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, "#category")))
+    WebDriverWait(browser, 2 ).until(
+        method=EC.title_is("Your Store"),
+        message="Не дождался корректного тайтла страницы"
+    )
 
 
-# def test_check_prices(browser, base_url):
-#     browser.get(f"{base_url}:8081")
-#     WebDriverWait(browser, 2).until(
-#         EC.element_to_be_clickable((By.CSS_SELECTOR, "#form-currency"))
-#     ).click()
-#     euro = browser.find_element(By.CSS_SELECTOR, "a[href='EUR']")
-#     euro.click()
-#     price_element = WebDriverWait(browser, 3).until(
-#         EC.visibility_of_element_located((By.CSS_SELECTOR, ".price"))
-#     )
-#     price_text = price_element.text
-#     assert "€" in price_text
+def test_administration_elements(browser, base_url):
+    browser.get(f"{base_url}:8081/administration/")
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-username")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-password")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#form-login > div.text-end > button")))
 
+
+def test_registration_elements(browser, base_url):
+    browser.get(f"{base_url}:8081/index.php?route=account/register")
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-firstname")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-email")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-password")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#input-newsletter")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#form-register > div > button")))
+
+
+def test_product_elements(browser, base_url):
+    browser.get(f"{base_url}:8081/en-gb/product/apple-cinema")
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(1) > div > a > img")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(1) > div")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(2) > form > div > button:nth-child(1)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#content > div.row.mb-3 > div:nth-child(2) > form > div > button:nth-child(2)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#button-cart")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR,
+         "#content > div.row.mb-3 > div:nth-child(2) > ul:nth-child(3) > li:nth-child(2) > h2 > span")))
+    WebDriverWait(browser, 2 ).until(
+        method=EC.title_is("Apple Cinema 30"),
+        message="Не дождался корректного тайтла страницы"
+    )
+
+
+def test_catalog_elements(browser, base_url):
+    browser.get(f"{base_url}:8081/en-gb/catalog/")
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(1)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(2)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(3)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(4)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(5)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(6)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(7)")))
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#column-left > div.list-group.mb-3 > a:nth-child(8)")))
+
+
+def test_login_logout_administration(browser, base_url):
+    browser.get(f"{base_url}:8081/administration/")
+    input_username = browser.find_element(By.ID, "input-username")
+    input_username.send_keys("user")
+    input_password = browser.find_element(By.ID, "input-password")
+    input_password.send_keys("bitnami")
+    browser.find_element(By.CSS_SELECTOR, "#form-login > div.text-end > button").click()
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "#nav-profile > a > span")))
+    browser.find_element(By.CSS_SELECTOR, "#nav-logout > a > span").click()
+
+
+def test_check_prices(browser, base_url):
+    browser.get(f"{base_url}:8081")
+    WebDriverWait(browser, 2).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "#form-currency"))
+    ).click()
+    euro = browser.find_element(By.CSS_SELECTOR, "a[href='EUR']")
+    euro.click()
+    price_element = WebDriverWait(browser, 3).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".price"))
+    )
+    price_text = price_element.text
+    assert "€" in price_text
+
+
+def test_add_to_cart_top(browser, base_url):
+    browser.get(f"{base_url}:8081")
+    selectors = [
+        "#content > div.row.row-cols-1.row-cols-sm-2.row-cols-md-3.row-cols-xl-4 > div:nth-child(1) > div > div.content > form > div > button:nth-child(1)",
+        "#content > div.row.row-cols-1.row-cols-sm-2.row-cols-md-3.row-cols-xl-4 > div:nth-child(2) > div > div.content > form > div > button:nth-child(1)"
+    ]
+    random_selector = random.choice(selectors)
+    add_to_cart = browser.find_element(By.CSS_SELECTOR, random_selector)
+    ActionChains(browser).move_to_element(add_to_cart).perform()
+    WebDriverWait(browser, 10).until(
+        EC.all_of(EC.visibility_of_element_located((By.CSS_SELECTOR, random_selector)),
+                  EC.element_to_be_clickable((By.CSS_SELECTOR, random_selector)))
+    )
+    browser.find_element(By.CSS_SELECTOR, random_selector).click()
+
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#alert > div"))
+    )
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#alert > div > button"))
+    ).click()
+
+    cart = browser.find_element(By.CSS_SELECTOR, "#header-cart .btn.btn-lg.btn-inverse")
+    ActionChains(browser).move_to_element(cart).perform()
+    cart.click()
+
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "img.img-thumbnail"))
+    )
